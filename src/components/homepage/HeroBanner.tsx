d1edc247369a779b1a0f54c2565d1225057ee5fc
payalptoliya416 <payal.rising416@gmail.com>
2026-07-31 09:25:58 +0530

    border radius

--- src/components/homepage/HeroBanner.tsx
+++ src/components/homepage/HeroBanner.tsx
@@ -5,9 +5,10 @@ function HeroBanner() {
   return (
     <>
       <div className="px-4 md:px-[30px]">
-        <div className="relative min-h-[765px] w-full flex flex-col justify-between p-4 md:p-8 text-white rounded-[22px]">
-          <div className="absolute inset-0 -z-10 bg-[url('/images/home-hero-bg.jpg')] bg-cover bg-center" />
-          <div className="absolute inset-0 -z-10 bg-gradient-to-b from-[#0F172ABF] via-[#0F172A8C] to-[#0F172ABF]" />
+        <div className="relative min-h-[765px] w-full flex flex-col justify-between p-4 md:p-8 text-white
+         rounded-[22px]">
+          <div className="absolute inset-0 -z-10 bg-[url('/images/home-hero-bg.jpg')] bg-cover bg-center rounded-[22px]" />
+          <div className="absolute inset-0 -z-10 bg-gradient-to-b from-[#0F172ABF] via-[#0F172A8C] to-[#0F172ABF]  rounded-[22px]" />
           <div className="flex flex-col items-center mx-auto my-auto">
             <div className="inline-flex items-center gap-2 backdrop-blur-md text-sm text-[#FFFFFFE5] border border-[#FFFFFF33] bg-[#FFFFFF1A] rounded-[999px] py-[10px] px-[15px] mb-6">
               <span className="h-2 w-2 rounded-full bg-amber-500 animate-pulse"></span>
@@ -47,7 +48,7 @@ function HeroBanner() {
           <button className="flex items-center justify-start gap-2 md:gap-[15px] rounded-full border border-[#FFFFFF33] bg-[#FFFFFF1A] px-3 py-2 text-xs font-light backdrop-blur-md transition hover:bg-[#FFFFFF33] md:text-sm">
             📅 <span>Events</span>
           </button>
-        </div>
+            </div>
           </div>
         </div>
       </div>
