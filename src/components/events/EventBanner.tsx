db0b4ab64cfa2f0ae2af1558e797b08484fdc2b4
payalptoliya416 <payal.rising416@gmail.com>
2026-07-31 15:59:53 +0530

    hero banner rounded

--- src/components/events/EventBanner.tsx
+++ src/components/events/EventBanner.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
 import BookingSearch from '../Booking/BookingSearch'
 
 function EventBanner() {
   return (
     <div className="px-4 md:px-[30px]">
         <div className="relative min-h-[623px] w-full flex flex-col justify-between p-4 md:p-8 text-white rounded-[22px]">
-          <div className="absolute inset-0 -z-10 bg-[url('/images/destination-banner.png')] bg-cover bg-center" />
-          <div className="absolute inset-0 -z-10 bg-gradient-to-b from-[#0F172ABF] via-[#0F172A8C] to-[#0F172ABF]" />
+          <div className="absolute inset-0 -z-10 bg-[url('/images/destination-banner.png')] bg-cover bg-center rounded-[22px]" />
+          <div className="absolute inset-0 -z-10 bg-gradient-to-b from-[#0F172ABF] via-[#0F172A8C] to-[#0F172ABF] rounded-[22px]" />
           <div className="flex flex-col items-center mx-auto my-auto">
             <div className="hero-tag">
               <span className="h-2 w-2 rounded-full bg-amber-500 animate-pulse"></span>
